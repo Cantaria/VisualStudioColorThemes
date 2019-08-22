95e5e6b997cbe6dd6af182ba98e1de171cd23f27
Clemens <cl.mattner@gmail.com>
2019-08-22 20:27:54 +0200

    AssemblyInfo added

---
+++ X3-Code_Blue/X3-Code_Blue/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("X3-Code Blue")]
+[assembly: AssemblyDescription("Blue Visual Studio Theme")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("X3-Code")]
+[assembly: AssemblyProduct("X3_Code_Blue.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e8ac9b4-3e33-4756-bfd7-7be8e07452ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
